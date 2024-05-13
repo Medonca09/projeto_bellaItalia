dbb9c475f0d900bd08bc127f4f3bea3a7598656a
Medonca09 <lucasmm0908@gmail.com>
2024-05-12 21:39:34 -0300

    adicionando projeto ao gitHub

---
+++ home.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>BellaItalia</title>
+</head>
+
+<body>
+    <div class="nav">
+        <div class="navBar">
+            <h1>BELLAITALIA TOURS</h1>
+            <div class="links">
+                <a href="home.php">Home</a>
+                <a href="#news">Sobre</a>
+                <a href="#contact">Pacotes</a>
+            </div>
+        </div>
+    </div>
+    <section class="carrosel">
+        <button class="slide-arrow" id="slide-arrow-prev">
+            &#8249;
+        </button>
+        <button class="slide-arrow" id="slide-arrow-next">
+            &#8250;
+        </button>
+        <ul class="slides-container" id="slides-container">
+            <li class="slide"><img src="Assets/coliseu.jpg" alt=""></li>
+            <li class="slide"><img src="Assets/milao.jpg" alt=""></li>
+            <li class="slide"><img src="Assets/veneza.jpg" alt=""></li>
+            <li class="slide"><img src="Assets/imagem.jpg" alt=""></li>
+            <li class="slide"><img src="Assets/piza.jpg" alt=""></li>
+        </ul>
+    </section>
+
+    <section>
+        <div>hello eword</div>
+    </section>
+
+    <script src="carrosel.js"></script>
+</body>
+
+</html>
