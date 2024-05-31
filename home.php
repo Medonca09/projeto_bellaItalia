de0a0b2264f587d56332ba81ce3e928013e6c610
Medonca09 <lucasmm0908@gmail.com>
2024-05-31 19:15:53 -0300

    Adicionando e estilizando novos card

--- home.php
+++ home.php
@@ -37,13 +37,15 @@
     </section>
 
     <section>
-        <h1>DESTINOS MAIS DESEJADOS</h1>
+        <div class="destinos">
+            <h1>OS DESTINOS MAIS DESEJADOS</h1>
+        </div>
         <div class="container">
             <div class="card">
                 <img src="Assets/destinoRoma.jpg" alt="Roma">
                 <div class="info">
                     <h2>Roma</h2>
-                    <p>Roma é a capital da Itália e uma das cidades mais antigas do mundo, cheia de história e cultura.</p>
+                    <p>Roma é a capital da Itália e uma das cidades mais antigas do mundo, cheia de história e cultura. É conhecida por seus monumentos icônicos...</p>
                 </div>
                 <a href="pacotes.php" class="btn">Comprar $</a>
             </div>
@@ -51,7 +53,7 @@
                 <img src="Assets/destinoVeneza.jpg" alt="Veneza">
                 <div class="info">
                     <h2>Veneza</h2>
-                    <p>Veneza é famosa por seus canais, gôndolas e sua arquitetura única.</p>
+                    <p>Veneza é famosa por seus canais, gôndolas e sua arquitetura única. Esta cidade encantadora, construída sobre uma lagoa, é conhecida por...</p>
                 </div>
                 <a href="pacotes.php" class="btn">Comprar $</a>
             </div>
@@ -59,7 +61,47 @@
                 <img src="Assets/destinoFlorenza.jpg" alt="Florença">
                 <div class="info">
                     <h2>Florença</h2>
-                    <p>Florença é conhecida por sua arte renascentista e monumentos históricos.</p>
+                    <p>Florença é conhecida por sua arte renascentista e monumentos históricos. Como berço do Renascimento, Florença abriga tesouros...</p>
+                </div>
+                <a href="pacotes.php" class="btn">Comprar $</a>
+            </div>
+            <div class="card">
+                <img src="Assets/destinoVaticano.jpg" alt="vaticano">
+                <div class="info">
+                    <h2>Vaticano</h2>
+                    <p>A Cidade do Vaticano, situada em Roma, é o menor país do mundo. Sede da Igreja Católica, abriga a Basílica de São Pedro, a Capela Sistina e os Museus Vaticanos...</p>
+                </div>
+                <a href="pacotes.php" class="btn">Comprar $</a>
+            </div>
+            <div class="card">
+                <img src="Assets/destinoVerona.jpg" alt="verona">
+                <div class="info">
+                    <h2>Verona</h2>
+                    <p>Verona, localizada na região do Vêneto, é uma cidade encantadora conhecida pela sua rica história e beleza arquitetônica.</p>
+                </div>
+                <a href="pacotes.php" class="btn">Comprar $</a>
+            </div>
+            <div class="card">
+                <img src="Assets/destnoTurim.jpg" alt="turim">
+                <div class="info">
+                    <h2>Turim</h2>
+                    <p>Turim, localizada no noroeste da Itália, é a capital da região do Piemonte e uma cidade rica em história e cultura. Conhecida por sua arquitetura elegante e pelas praças majestosas...</p>
+                </div>
+                <a href="pacotes.php" class="btn">Comprar $</a>
+            </div>
+            <div class="card">
+                <img src="Assets/destinoMilao.jpg" alt="milão">
+                <div class="info">
+                    <h2>Milão</h2>
+                    <p>Milão, a capital da moda e do design da Itália, é uma cidade vibrante e moderna localizada na região da Lombardia. Famosa por sua influência no mundo da moda...</p>
+                </div>
+                <a href="pacotes.php" class="btn">Comprar $</a>
+            </div>
+            <div class="card">
+                <img src="Assets/destinoNapoles.jpg" alt="napoles">
+                <div class="info">
+                    <h2>Napoles</h2>
+                    <p>Nápoles, situada na costa sudoeste da Itália, é a capital da região da Campânia e uma das cidades mais antigas e culturalmente ricas do país...</p>
                 </div>
                 <a href="pacotes.php" class="btn">Comprar $</a>
             </div>
